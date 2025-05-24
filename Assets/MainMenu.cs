cad8e47b39462067ea798839b07b614df824d12e
benaliYacine <benaliyacine324@gmail.com>
2025-05-24 08:12:50 +0100

    moved sounds folder to assets

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -1,9 +1,15 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenu : MonoBehaviour
-{
-    public void PlayGame(int sceneIndex){
+public class MainMenu : MonoBehaviour {
+    public AudioSource menuMusic;
+
+    void Start() {
+        menuMusic.loop = true;
+        menuMusic.Play();
+    }
+
+    public void PlayGame(int sceneIndex) {
         SceneManager.LoadSceneAsync(sceneIndex);
     }
     public void QuitGame(){
